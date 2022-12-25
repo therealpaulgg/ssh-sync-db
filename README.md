60056c294fcb36b9a05ec11cbc2996021ea99b27
therealpaulgg <paul@paul.systems>
2022-12-24 21:23:22 -0700

    tables

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ssh-sync-db
+# Postgres Environment
+
+This is useful for development and production.
+
+To use, clone the repository, and simply run:
+
+`docker-compose up`
+
+It will create all tables automatically and you will have a database running on `127.0.0.1:5432`. :)
+
+If you want to ensure it is properly reset: `docker-compose up --build`
---
+++ init.sql
@@ -0,0 +1,45 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+/* Table 'users' */
+CREATE TABLE IF NOT EXISTS users(
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    username VARCHAR(255) NOT NULL unique,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS machines(
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    user_id uuid NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS master_keys(
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    user_id uuid NOT NULL,
+    machine_id uuid NOT NULL,
+    data BYTEA NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (machine_id) REFERENCES machines(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS ssh_config(
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    user_id uuid NOT NULL,
+    machine_id uuid NOT NULL,
+    host VARCHAR(255) NOT NULL,
+    values JSON NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS ssh_keys(
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    user_id uuid NOT NULL,
+    filename VARCHAR(255) NOT NULL,
+    data BYTEA NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
