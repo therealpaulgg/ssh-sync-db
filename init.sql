c2b6267c72cd2f6411b2f378ed48b8af1aeeff34
therealpaulgg <paul@paul.systems>
2023-01-18 00:05:43 -0700

    no more master keys

--- init.sql
+++ init.sql
@@ -17,18 +17,6 @@ CREATE TABLE IF NOT EXISTS machines(
     unique (user_id, name)
 );
 
-CREATE TABLE IF NOT EXISTS master_keys(
-    id uuid DEFAULT uuid_generate_v4() NOT NULL,
-    user_id uuid NOT NULL,
-    machine_id uuid NOT NULL,
-    data BYTEA NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (machine_id) REFERENCES machines(id),
-    unique (user_id, machine_id)
-);
-
-
 CREATE TABLE IF NOT EXISTS ssh_configs(
     id uuid DEFAULT uuid_generate_v4() NOT NULL,
     user_id uuid NOT NULL,
